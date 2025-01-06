bf97902c61ad2a02c0179355cfb1ab0f413347b9
Tanishq510 <tanishqjoshi93@gmail.com>
2025-01-06 10:14:25 +0530

    Two sum solution

---
+++ array/two-sum.js
@@ -0,0 +1,20 @@
+//The objet is to find the subarray which adds up to the required sum
+const sum = 10
+const number = 5
+let anotherNumber  
+const array = [1,2,3,4,5]
+
+//Solution one
+array.forEach((arr)=>{
+   if(arr+number === sum) {
+    anotherNumber = number
+   }
+})
+
+//Solution two
+const diff = sum-number
+const exists = array.indexOf(diff)
+
+if(exists !==-1){
+    anotherNumber = diff
+}
